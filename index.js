202673174cec605c5acec7bb4e944afff1059af4
MrCesar107 <cesar.rodriguez.lara54@gmail.com>
2019-06-28 20:03:35 -0500

    User login with token

--- index.js
+++ index.js
@@ -1,74 +1,107 @@
+// cargando las variables de entorno - process.env.URL_DATABASE
 require('dotenv').config()
 
-const {ApolloServer, gql} = require('apollo-server')
-const mongoose = require('mongoose');
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require("mongoose");
+const { UserModel } = require('./app/src/dataBase/models');
+const bcrypt = require('bcrypt');
+const { createToken } = require('./app/src/actions/authActions');
 
-const { UserModel } = require('./app/src/dataBase/models')
-
+// parametros de conexion a la base de datos
 mongoose.connect(
-    process.env.URL_DATABASE,
-    {
-        userCreateIndex: true,
-        userNewUrlParser: true,
-    }
-);
-
+  process.env.URL_DATABASE,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+)
+// crea la conexion a la base de datos
 const mongo = mongoose.connection;
-// Listeners
-mongo.on('error', console.error.bind('Error de conexion'))
+// listeners, 
+mongo.on('error', console.error.bind(console, 'Error de conexion'))
 mongo.on('open', () => console.log('Conectado !'))
 
-// Esquema de graphql
+// esquema de graphql
+// type: para queries
+// input: para mutations
+// Mutations -- Resolver : la respuesta
 const typeDefs = gql`
   type Auth {
+    token: String!
     message: String
   }
-
   input UserInput {
     name: String!
     lastName: String!
     email: String!
     password: String!
   }
-
   type Query {
     newQuery: Auth
   }
-
   type Mutation {
     signup(data: UserInput): Auth
+    login(email: String!, password: String!): Auth
   }
-`
-
-// Resolvers
+`;
 
 const resolvers = {
   Query: {
     newQuery: () => {
-      return { message: "Este es un mensaje de prueba" }
+      return { message: 'Este es un mensaje de prueba' }
     }
   },
-  // Parent
+  // resolvers: funciones,
+  // parent --- 
   // args -- argumentos que recibe la funcion
   // context -- se comparte atravez de todos los resolvers
-  // info
+  // info 
   Mutation: {
-    signup: (parent, args, context, info) => {
+    signup: (paret, args, context, info) => {
       return UserModel.create({ ...args.data }).then(() => {
-        return { message: `Se ha registrado el usuario ${args.data.name}` }
+        return { message: `se ha registrado el usuario ${args.data.name}` }
       }).catch(err => {
-        return { message: `No se registro al usuario ${args.data.name}` }
+        return { message: `${err}` }
+      })
+    },
+    login: (parent, args, context, info) => {
+      return loginAction({ email: args.email, password: args.password }).then(Auth => {
+        console.log('auth', Auth)
+        return Auth;
+      }).catch(error => {
+        console.log('ERROR', error)
+        return {
+          token: '',
+          message: `${error}`
+        };
       })
     }
   }
 }
 
-// Levantando el servidor
+const loginAction = (data) => {
+  return new Promise((resolve, reject) => {
+    UserModel.findOne({ email: data.email }).then(user => {
+      console.log('errir', user.password, data.password)
+
+      bcrypt.compare(
+        data.password,
+        user.password,
+        (err, isValid) => {
+          if (err) reject({ token: '', message: `${err}` });
+          isValid
+            ? resolve({ token: createToken(user),
+                        message: `se ha logueado correctamente` })
+            : reject({ token: '', message: `credenciales invalidas` })
+        })
+    })
+  });
+}
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
-server.listen().then(({url}) => {
-  console.log(`Server running in por ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`server correindo en ${url}`);
+});
