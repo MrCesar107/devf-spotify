89251f355380fbe15cd519d7d593328f8e16c986
MrCesar107 <cesar.rodriguez.lara54@gmail.com>
2019-06-28 20:59:23 -0500

    Last commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 require('dotenv').config()
 
 const { ApolloServer, gql } = require('apollo-server');
+const { SchemaDirectiveVisitor, AuthenticacionError } = require('apollo-server-express')
 const mongoose = require("mongoose");
 const { UserModel } = require('./app/src/dataBase/models');
+const JWT = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const { createToken } = require('./app/src/actions/authActions');
 
@@ -26,19 +28,25 @@ mongo.on('open', () => console.log('Conectado !'))
 // input: para mutations
 // Mutations -- Resolver : la respuesta
 const typeDefs = gql`
+  directive @AuthDirective on QUERY | FIELD_DEFINITION | FIELD
   type Auth {
     token: String!
     message: String
   }
+
   input UserInput {
     name: String!
     lastName: String!
     email: String!
     password: String!
   }
+
   type Query {
     newQuery: Auth
+    messageWithLogin: String @AuthDirective
+    publicMessage: String
   }
+
   type Mutation {
     signup(data: UserInput): Auth
     login(email: String!, password: String!): Auth
@@ -49,6 +57,14 @@ const resolvers = {
   Query: {
     newQuery: () => {
       return { message: 'Este es un mensaje de prueba' }
+    },
+
+    messageWithLogin: () => {
+      return { message: "Este es un mensaje de un usuario autenticado" }
+    },
+
+    publicMessage: () => {
+      return { message: "Este es un mensaje para todos" }
     }
   },
   // resolvers: funciones,
@@ -57,7 +73,7 @@ const resolvers = {
   // context -- se comparte atravez de todos los resolvers
   // info 
   Mutation: {
-    signup: (paret, args, context, info) => {
+    signup: (parent, args, context, info) => {
       return UserModel.create({ ...args.data }).then(() => {
         return { message: `se ha registrado el usuario ${args.data.name}` }
       }).catch(err => {
@@ -88,7 +104,7 @@ const loginAction = (data) => {
         data.password,
         user.password,
         (err, isValid) => {
-          if (err) reject({ token: '', message: `${err}` });
+          if (err) reject({ token: '', message: `${err}` })
           isValid
             ? resolve({ token: createToken(user),
                         message: `se ha logueado correctamente` })
@@ -97,11 +113,43 @@ const loginAction = (data) => {
     })
   });
 }
+
+const getContext = (req) => {
+  const token = req.headers.authorization
+
+  if (typeof token === typeof undefined) return req
+  return JWT.verify(token, process.env.SECRET, function(err, result) {
+    if(err) return req;
+    return UserModel.findOne({_id: result._id}).then(user => {
+      console.log(user)
+      return { ...req, user }
+    })
+  })
+}
+
+class AuthDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field) {
+    const { resolve = defaultFieldResolver } = field;
+    field.resolve = async function (...args) {
+      const ctx = args[2];
+      if (ctx.user) {
+        return await resolve.apply(this, args);
+      } else {
+        throw new AuthenticationError("neceistas estar logueado para hacer esto")
+      }
+    }
+  }
+}
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  schemaDirectives: {
+    AuthDirective: AuthDirective
+  },
+  context: async({req}) => getContext(req)
 });
 
 server.listen().then(({ url }) => {
-  console.log(`server correindo en ${url}`);
+  console.log(`server correindo en ${url}`)
 });
